d8667e6a69560c3dc074f492bbda8c21eb09cc6c
kuzznicki <kuznicki@protonmail.com>
2022-12-05 18:02:13 +0100

    style multiselect

--- src/components/MultiSelect.tsx
+++ src/components/MultiSelect.tsx
@@ -1,6 +1,6 @@
 import Select from 'react-select';
 import '@/styles/MultiSelect.scss';
-
+import cssVars from '@/styles/_export.module.scss';
 
 type SelectProps = {
     options: { value: string, label: string }[],
@@ -16,6 +16,18 @@ export default function MultiSelect({ options, placeholder, onChange }: SelectPr
             placeholder={placeholder}
             options={options}
             onChange={options => onChange(options.map(o => o.value))}
+            theme={theme => ({
+                ...theme,
+                colors: {
+                    ...theme.colors,
+                    primary: cssVars.inputFocusColor,
+                    primary25: cssVars.secondaryBluePale10,
+                    primary50: cssVars.secondaryBluePale30,
+                    neutral10: cssVars.primaryAnthracite80,
+                    neutral80: cssVars.primaryAnthracite80,
+                    neutral20: cssVars.inputBorderColor,
+                }
+            })}
             classNames={{
                 container: () => 'multi-select',
                 control: () => 'control',
