918b062784e3df30e6d6aa58d08fbb90a0099a70
kuzznicki <kuznicki@protonmail.com>
2022-12-07 16:04:17 +0100

    update 'base' in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/rick-and-morty-app',
   plugins: [react(), tsconfigPaths()]
 })
